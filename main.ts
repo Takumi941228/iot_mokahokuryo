c7b429ff54681ebe854b97812e00b7274eddf25c
Takumi Hirota <96970720+Takumi941228@users.noreply.github.com>
2022-01-17 15:17:00 +0900

    commit

--- main.ts
+++ main.ts
@@ -1,9 +1,18 @@
+/**
+ * 受信したデータをシリアル通信にて、コンソール画面に出力する。
+ */
 radio.onReceivedValue(function (name, value) {
     serial.writeValue(name, value)
     serial.writeValue(name, value)
 })
+/**
+ * 無線のグループを1にする。
+ */
 radio.setGroup(1)
 radio.setTransmitPower(7)
+/**
+ * 温度及び明るさを無線で1秒ごとに送信する。
+ */
 basic.forever(function () {
     radio.sendValue("light", input.lightLevel())
     radio.sendValue("temp", input.temperature())
