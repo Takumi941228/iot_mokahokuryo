045118d53b4887f15bb86b13bfe92f6337d9d10f
Takumi Hirota <96970720+Takumi941228@users.noreply.github.com>
2022-01-17 14:18:20 +0900

    commit

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-
+radio.onReceivedValue(function (name, value) {
+    serial.writeValue(name, value)
+    serial.writeValue(name, value)
+})
+radio.setGroup(1)
+radio.setTransmitPower(7)
+basic.forever(function () {
+    radio.sendValue("light", input.lightLevel())
+    radio.sendValue("temp", input.temperature())
+    basic.pause(1000)
+})
